aba69776d70b19a40244f79fe20e068e16fdde75
gursimran <gursimran@amits-macbook-pro-2.local>
2016-06-03 17:59:50 -0400

    Initial Commit

---
+++ weatherApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApplication
+//
+//  Created by gursimran on 2016-06-03.
+//  Copyright © 2016 gursimran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
